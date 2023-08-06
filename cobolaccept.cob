f7ff17af56797b6b7a3c3715fb4392298694343c
defycab <love.bellinder@gmail.com>
2023-08-06 19:51:07 +0200

    add spaces literals

--- cobolaccept.cob
+++ cobolaccept.cob
@@ -8,8 +8,8 @@
            DATA DIVISION.
            FILE SECTION.
            WORKING-STORAGE SECTION.
-           01 FirstName pic x(30) value " ".
-           01 LastName pic x(30) value " ".
+           01 FirstName pic x(5) value spaces.
+           01 LastName pic x(30) value spaces.
            01 WholeName pic x(60).
       
            PROCEDURE DIVISION.
@@ -17,7 +17,7 @@
            accept FirstName
            display "Efternamn ? " with no advancing
            accept LastName
-           display FirstName " " LastName
+           display FirstName LastName
            
            STOP RUN.
       
